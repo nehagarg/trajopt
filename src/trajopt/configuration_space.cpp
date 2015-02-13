5e0662106f89f4992f89aa25bbb5280cd88798da
Pras <psigen@gmail.com>
2015-02-13 15:46:20 -0500

    Reverted DOF index check.. I think it's actually OK.

--- src/trajopt/configuration_space.cpp
+++ src/trajopt/configuration_space.cpp
@@ -35,7 +35,7 @@ DblVec RobotAndDOF::GetDOFValues() {
 void RobotAndDOF::SetRobotActiveDOFs()  {
   RobotBasePtr robot1 = boost::dynamic_pointer_cast<RobotBase>(robot); // since robot is just a kinbody
   vector<int> current_active = robot1->GetActiveDOFIndices();
-  if (!std::equal(current_active.begin(), current_active.end(), joint_inds.begin()))
+  if (robot1->GetActiveDOF() != GetDOF() || !std::equal(current_active.begin(), current_active.end(), joint_inds.begin()))
     robot1->SetActiveDOFs(joint_inds, affinedofs);
 }
 
