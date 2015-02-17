c45597119244fecf28b143fc4f2e7c41a1c5c668
Pras <psigen@gmail.com>
2015-02-17 18:03:07 -0500

    Updated check_traj for PEP8 and newer OpenRAVE API.

--- trajoptpy/check_traj.py
+++ trajoptpy/check_traj.py
@@ -1,24 +1,25 @@
 import trajoptpy.math_utils as mu
-import openravepy as rave
 import numpy as np
+
 
 def traj_collisions(traj, robot, n=100):
     """
-    Returns the set of collisions. 
+    Returns the set of collisions.
     manip = Manipulator or list of indices
     """
-    traj_up = mu.interp2d(np.linspace(0,1,n), np.linspace(0,1,len(traj)), traj)
-    _ss = rave.RobotStateSaver(robot)
-    
+    traj_up = mu.interp2d(np.linspace(0, 1, n),
+                          np.linspace(0, 1, len(traj)), traj)
     env = robot.GetEnv()
     col_times = []
-    for (i,row) in enumerate(traj_up):
-        robot.SetActiveDOFValues(row)
-        col_now = env.CheckCollision(robot)
-        if col_now: 
-            col_times.append(i)
-    return col_times
-    
+
+    with robot:
+        for (i, row) in enumerate(traj_up):
+            robot.SetActiveDOFValues(row)
+            col_now = env.CheckCollision(robot)
+            if col_now:
+                col_times.append(i)
+        return col_times
+
 
 def traj_is_safe(traj, robot, n=100):
-    return traj_collisions(traj, robot, n) == []
+    return len(traj_collisions(traj, robot, n)) == 0
